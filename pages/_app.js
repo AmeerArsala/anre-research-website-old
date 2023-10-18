29527d5a833857d8b5149784137ea3da9bba9326
Ameer Arsala <56413920+AmeerArsala@users.noreply.github.com>
2023-10-18 02:34:00 +0000

    added site icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/anre-temp.jpg"/>
+      </Head>
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
