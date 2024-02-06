35fcfe64f6ecfeafe480b1bf8c55575ad2a756fc
Ameer Arsala <56413920+AmeerArsala@users.noreply.github.com>
2024-02-05 20:32:01 -0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,7 @@ import styles from "./Header.module.css";
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      {/* ANRE RESEARCH ICON */}
-      <div>
-        <Image className={styles.circularImage} src="/anre-temp.jpg" width={100} height={100}/>
-      </div>
-
-      {/* <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
+      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
 
       <p className="text-2xl dark:text-white text-center drop-shadow">
         <Link href="/">
